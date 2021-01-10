bff5bb10b4eacc9b3b8e98329eff39338b5dfc23
Hack-Net998 <77234932+Hack-Net998@users.noreply.github.com>
2021-01-10 20:54:17 +0700

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,31 @@
+import os.sys.time
+
+x = "Hack"
+y = "Net"
+
+def login ():
+
+            print "¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦"
+           print "¦¦¦¦¦   ¦¦¦¦¦¦¦¦¦¦¦   ¦¦¦
+          print "¦¦¦¦¦ ¦¦¦ ¦¦¦¦¦¦¦¦¦ ¦¦¦ ¦¦¦"
+         print "¦¦¦¦¦¦ ¦¦¦ ¦¦¦¦¦¦¦¦¦ ¦¦¦ ¦¦¦¦"
+        print "¦¦¦¦¦¦¦ ¦¦¦ ¦¦¦¦¦¦¦¦¦ ¦¦¦ ¦¦¦¦¦"
+       print "¦¦¦¦¦¦¦¦¦   ¦¦¦¦¦¦¦¦¦¦¦   ¦¦¦¦¦¦¦"
+      print "¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦"
+       print "¦¦¦¦¦¦ ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦ ¦¦¦¦"
+        print "¦¦¦¦¦¦¦ ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦ ¦¦¦¦¦"
+         print "¦¦¦¦¦¦¦¦ ¦¦¦¦¦¦¦¦¦¦¦¦¦ ¦¦¦¦¦¦"
+          print "¦¦¦¦¦¦¦¦¦           ¦¦¦¦¦¦¦"
+           print "¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦"
+           print "Welcome To Hack Net"
+
+    user = raw_input("Username : ")
+    pasw = raw_input("Password : ")
+    if user ==x and ==y:
+       print "Token Is Work"
+       time.sleep(2)
+       os.system("python2 login.py")
+    else:
+        print"Token Not Work"
+if __name__ == "__main__":
+         login():
